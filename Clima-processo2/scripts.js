6fcd04d3f22744e960d6cf515524f7dc388d23bd
eduhernandes <eduardo.hernandes@hotmail.com>
2024-04-18 20:28:18 -0300

    atu

--- Clima-processo2/scripts.js
+++ Clima-processo2/scripts.js
@@ -16,8 +16,18 @@ const windElement = document.querySelector("#wind span");
 
 
 //Funções
+const getWeatherData = async(city) =>{
+    const apiWheatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWheatherURL);
+    const data = await res.json();
+
+    console.log(data);
+};
+
 const showWeatherData = (city) => {
     console.log(city);
+    getWeatherData(city);
 };
 
 
